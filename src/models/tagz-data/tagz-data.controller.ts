fd90ca5a2900ed065eaf95d4446eef76208a5773
Nicky <fatemesahraee2000@gmail.com>
2023-04-16 14:36:45 +0330

    port changed

--- src/models/tagz-data/tagz-data.controller.ts
+++ src/models/tagz-data/tagz-data.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
-import { TagzDataService } from "models/tagz-data/tagz-data.service";
-import { JwtGuard } from "@/models/auth/guard";
-import { GetUser } from "@/models/auth/decorator";
-import { PartialUser } from "@/models/user/entities";
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { TagzDataService } from 'models/tagz-data/tagz-data.service';
+import { JwtGuard } from '@/models/auth/guard';
+import { GetUser } from '@/models/auth/decorator';
+import { PartialUser } from '@/models/user/entities';
 
 @UseGuards(JwtGuard) // a guard for the controller
 @Controller('tagz-data')
@@ -35,21 +35,21 @@ export class TagzDataController {
     };
   }
 
-  @Get("locations")
+  @Get('locations')
   async getLocations(@GetUser() user: PartialUser) {
     const data = await this.tagzDataService.getLocations(user.username);
     return {
       data,
-      message: "ok"
+      message: 'ok',
     };
   }
 
-  @Get("balanceUsages")
+  @Get('balanceUsages')
   async getBalanceUsages(@GetUser() user: PartialUser) {
     const data = await this.tagzDataService.getBalanceUsages(user.username);
     return {
       data,
-      message: "ok"
+      message: 'ok',
     };
   }
 }
